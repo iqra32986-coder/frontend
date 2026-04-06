5314dec124973cc825d988cf5235d5d1459f5301
iqra32986-coder <iqra32986@gmail.com>
2026-04-06 06:45:52 -0700

    Change API base URL to production endpoint

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Adjust base URL as needed
+  baseURL: 'https://food-court-backend-one.vercel.app/', // Adjust base URL as needed
 });
 
 api.interceptors.request.use((config) => {
